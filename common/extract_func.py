61f5b1318889cffe7c8cf2654290c50222ba0556
Anne Eiken <annewei@stud.ntnu.no>
2020-10-21 15:36:01 +0200

    from csv to parquet

--- common/extract_func.py
+++ common/extract_func.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 def extract_func(country_name, info_type, filepath=''):
-    data = pd.read_csv(filepath+'df_lookup_table.csv')
-    return data.loc[data.Country == country_name, info_type].values[0]
+    data = pd.read_parquet(filepath+'df_lookup_table.parquet')
+    return data.loc[data.index == country_name, info_type].values[0]
    
   
 
