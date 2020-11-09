c0ca8cda08590c63e31d400dd907702dd45bcab7
Anne Eiken <annewei@stud.ntnu.no>
2020-11-09 20:37:57 +0100

    commenting more code

--- new_version/misc/cluster_mapping_func.py
+++ new_version/misc/cluster_mapping_func.py
@@ -3,30 +3,39 @@ import folium
 from folium.plugins import MarkerCluster
 from extract_func import extract_func
 
+#This function takes four list of countries (Intention is one list per cluster) as input and places these on a map using the folium package.
+#The input clusters are assigned the colors green, red, blue and orange respecitivly based on order clusters are 
+#given as argument to function.
+#Filepath can be given as argument to specify folder path from cluster_mapping to extract_func.
+
 def cluster_mapping_func(cluster_1,cluster_2,cluster_3,cluster_4, filepath=''):
     m = folium.Map(
-        location=[40.416775, -3.703790],
+        #coordinates of map center
+        location=[40, 1],
         #level of zoom when map is first displayed
-        zoom_start=2,
-        tiles='OpenStreetMap', 
-        #width='80%', 
+        zoom_start=1,
+        #map type
+        tiles='OpenStreetMap',  
     )
-
-    m.add_child(folium.LatLngPopup())
     
     color_list=['green','red','blue','orange']
-    i=0
+    counter=0
 
     for cluster in [cluster_1,cluster_2,cluster_3,cluster_4]:
-        color = color_list[i]
-        i+=1
+        #select color
+        color = color_list[counter]
+        counter+=1
         for country in cluster:
+            #extract latitude and longitude for each country from lookup table, using ectract function 
             lat = extract_func(country,'Latitude', filepath)
             lon = extract_func(country,'Longitude', filepath)
-    
+
+            #add country name to marker  
             popup = '<b>{}</b></br><i> </i>'.format(country)
+            #create icon in correct color and with circle icon 
             icon = folium.Icon(color=color,icon='circle')
-        
+            
+            #add marker to map
             folium.Marker([lat, lon], popup=popup,icon=icon).add_to(m)
     
     
--- new_version/misc/country_mapping_func.py
+++ new_version/misc/country_mapping_func.py
@@ -3,23 +3,28 @@ import folium
 from folium.plugins import MarkerCluster
 from extract_func import extract_func
 
+#This function takes list over all countries as input and places these on a map using the folium package.
+#Filepath can be given as argument to specify folder path from cluster_mapping to extract_func.
+
 def country_mapping_func(list_over_countries, filepath=''):
     m = folium.Map(
-        location=[40.416775, -3.703790],
+        #coordinates of map center
+        location=[40, 1],
         #level of zoom when map is first displayed
-        zoom_start=2,
+        zoom_start=1,
+        #map type
         tiles='OpenStreetMap', 
-        #width='80%', 
     )
 
-    m.add_child(folium.LatLngPopup())
-
     for country in list_over_countries:
+        #extract latitude and longitude for each country from lookup table, using ectract function 
         lat = extract_func(country,'Latitude', filepath)
         lon = extract_func(country,'Longitude', filepath)
-    
+        
+        #add country name to marker   
         popup = '<b>{}</b></br><i> </i>'.format(country)
     
+        #add marker to map
         folium.Marker([lat, lon], popup=popup).add_to(m)
     
     return m
